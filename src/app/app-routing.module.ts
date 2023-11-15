fef9c65378864efce822aa3cedd355394cec03c2
WaelFerjani <wael.ferjani@esprit.tn>
2023-11-15 20:08:38 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: '/dashboard',
+        redirectTo: '/dashboard',     
         pathMatch: 'full',
       },
       {
